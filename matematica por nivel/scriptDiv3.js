b3e917f550ec07c5095ca18623e7a922874b17d0
aragao-lucas <126295197+aragao-lucas@users.noreply.github.com>
2026-04-28 08:20:33 -0300

    Add files via upload

---
+++ matematica por nivel/scriptDiv3.js
@@ -0,0 +1,16 @@
+        i = 0
+        score = 0;
+        while (i < 10){        
+            
+        let choice1 = Math.floor(Math.random() * 1000);
+        let choice2 = Math.floor(Math.random() * 1000);
+        result = prompt("quanto é " + choice1 + " / " + choice2 + "?");
+        if (result == choice1 / choice2) {
+            alert("parabéns, você acertou!");
+            score ++;
+            document.getElementById("item2").innerHTML = "Pontuação: " + score;
+        } else {
+            alert("que pena, você errou! a resposta correta é " + (choice1 / choice2));
+        }
+        i ++;
+    }
