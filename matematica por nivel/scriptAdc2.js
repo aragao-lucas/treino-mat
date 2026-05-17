9c428fcc3524eabc6685b511ebbcef50add14d07
aragao-lucas <126295197+aragao-lucas@users.noreply.github.com>
2026-05-17 15:05:13 -0300

    Add files via upload

---
+++ matematica por nivel/scriptAdc2.js
@@ -0,0 +1,17 @@
+        i = 0
+        score = 0;
+        while (i < 10){        
+        
+        
+        let choice1 = Math.floor(Math.random()*101);
+        let choice2 = Math.floor(Math.random()*101);
+        result = prompt("quanto é " + choice1 + " + " + choice2 + "?");
+        if (result == choice1 + choice2) {
+            alert("parabéns, você acertou!");
+            score ++;
+            document.getElementById("item2").innerHTML = "Pontuação: " + score;
+        } else {
+            alert("que pena, você errou! a resposta correta é " + (choice1 + choice2));
+        }
+        i ++;
+    }
